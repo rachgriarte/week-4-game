5bdfe0ff79900cbb70141e36439917e69c2c409b
Rach Griarte <rachelgriarte@hotmail.com>
2017-06-13 18:55:43 -0400

    done

--- assets/javascript/celineInAction.js
+++ assets/javascript/celineInAction.js
@@ -1,73 +1,58 @@
 $(document).ready(function() {
-
 // Move Buttons
   $(".up-button").on("click", function() {
-    $(".celine-bag").animate({ top: "-=200px" }, "normal");
+    $(".celine-bag").animate({ top: "-=100px" }, "normal");
 });
 
   $(".down-button").on("click", function() {
-    $(".celine-bag").animate({ top: "+=200px" }, "normal");
+    $(".celine-bag").animate({ top: "+=100px" }, "normal");
 });
 
   $(".left-button").on("click", function() {
-    $(".celine-bag").animate({ left: "-=200px" }, "normal");
+    $(".celine-bag").animate({ left: "-=100px" }, "normal");
 });
 
   $(".right-button").on("click", function() {
-    $(".celine-bag").animate({ left: "+=200px" }, "normal");
+    $(".celine-bag").animate({ left: "+=100px" }, "normal");
 });
 
   $(".back-button").on("click", function() {
-    $(".celine-bag").animate({ top: "50px", left: "80px" }, "fast");
+    $(".celine-bag").animate({ top: "70px", left: "40px" }, "fast");
 });
 
 // Move Buttons (Keyboard Down)
   $(document).keyup(function(e) {
     switch (e.which) {
       case 40:
-        $(".celine-bag").animate({ top: "+=200px" }, "normal");
-    }
-});
-
-// Move Buttons (Keyboard Right)
-  $(document).keyup(function(e) {
-    switch (e.which) {
+        $(".celine-bag").animate({ top: "+=100px" }, "normal");
+        break;
       case 39:
-        $(".celine-bag").animate({ left: "+=200px" }, "normal");
-    }
-});
-
-// Move Buttons (Keyboard Up)
-  $(document).keyup(function(e) {
-    switch (e.which) {
+        $(".celine-bag").animate({ left: "+=100px" }, "normal");
+        break;
       case 38:
-        $(".celine-bag").animate({ top: "-=200px" }, "normal");
-    }
-});
-
-// Move Buttons (Keyboard Left)
-  $(document).keyup(function(e) {
-    switch (e.which) {
-      case 37:
-        $(".celine-bag").animate({ left: "-=200px" }, "normal");
+        $(".celine-bag").animate({ top: "-=100px" }, "normal");
+        break;
+       case 37:
+        $(".celine-bag").animate({ left: "-=100px" }, "normal");
+        break;
     }
 });
 
 // Stretch Buttons
   $(".stretch-btn").on("click", function() {
-    $(".celine-bag").animate({ height: "1000px", width: "200px"});
+    $(".celine-bag").animate({ height: "700px", width: "400px"});
 });
 
   $(".unstretch-btn").on("click", function() {
-    $(".celine-bag").animate({ height: "300px", width: "450px"});
+    $(".celine-bag").animate({ height: "100px", width: "250px"});
 });
 
 // Visibility Buttons
-  $(".vis-button").on("click", function() {
-    $(".celine-bag").animate({ opacity: "1" });
-});
+$(".vis-button").on("click", function() {
+        $(".celine-bag").animate({ opacity: "2" });
+      });
 
-  $(".invis-button").on("click", function() {
-    $(".celine-bag").animate({ opacity: "0.05" });
+      $(".invis-button").on("click", function() {
+        $(".celine-bag").animate({ opacity: "0.05" });
+      });
 });
-// });
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,124 +1,93 @@
-$(document).ready(function() {
+$( document ).ready(function(){
+  
+  var Random=Math.floor(Math.random()*101+19)
+  $("#randomNumber").text(Random);
+ 
+   // The random numbers for each bags
+  var num1= Math.floor(Math.random()*11+1)
+  var num2= Math.floor(Math.random()*11+1)
+  var num3= Math.floor(Math.random()*11+1)
+  var num4= Math.floor(Math.random()*11+1)
+  var userTotal= 0; 
+  var wins= 0;
+  var losses = 0;
+  
+$("#numbWins").text(wins);
+$("#numbLosses").text(losses);
 
-    var yourMatchingNumber = 0;
-
-
-    var randomNum = randomNumGen();
-
-    var wins = 0;
-    var losses = 0;
-    var gems;
-
-    function randomNumgems() {
-        // gems obj
-        return {
-            'red': {
-                points: Math.floor(Math.random() * 11) + 1,
-                imageUrl: "assets/images/egeme.png"
-            },
-            'blue': {
-                points: Math.floor(Math.random() * 11) + 1,
-                imageUrl: "assets/images/geme.png"
-            },
-            'yellow': {
-                points: Math.floor(Math.random() * 11) + 1,
-                imageUrl: "assets/images/bgem.png"
-            },
-            'green': {
-                points: Math.floor(Math.random() * 11) + 1,
-                imageUrl: "assets/images/icecreambar.gif"
-            }
-        };
-    }
-
-    function randomNumGen() {
-        return Math.floor(Math.random() * 100) + 18;
-    }
-
-    function setGame() {
-        yourMatchingNumber = 0;
-        //create random gem numbers
-        gems = randomNumgems();
-        //create a random number and render it
-        randomNum = randomNumGen();
-        var randomNumDiv = $("<div id='random-number'>").text(randomNum);
-        $("#random-area").html(randomNumDiv);
-    }
-
-    function updateDom(didUserWin) {
-        $('#winArea').empty();
-
-        if (didUserWin === true) {
-            $('#winArea').append($('<p>').text('You won!!'));
-            setGame();
-            renderMatchingNumber();
-        } else if (didUserWin === false) {
-            $('#winArea').append($('<p>').text('You lost!!'));
-            setGame();
-            renderMatchingNumber();
+//This will reset the game
+function reset(){
+      Random=Math.floor(Math.random()*101+19);
+      console.log(Random)
+      $("#randomNumber").text(Random);
+      num1= Math.floor(Math.random()*11+1);
+      num2= Math.floor(Math.random()*11+1);
+      num3= Math.floor(Math.random()*11+1);
+      num4= Math.floor(Math.random()*11+1);
+      userTotal= 0;
+      $("#finalTotal").text(userTotal);
+      } 
+//This adds the wins to the userTotal
+function yaywin(){
+  alert ('Good job, Shopper! You won!');
+  wins++; 
+  $("#numbWins").text(wins);
+  reset();
+}
+//This adds the losses to the userTotal
+function loser(){
+alert ('You failed...Try again, Shopper!');
+  losses++;
+  $("#numbLosses").text(losses);
+  reset()
+}
+//sets up click for bags
+  $("#one").on("click", function(){
+    userTotal = userTotal + num1;
+    console.log('New userTotal= ' + userTotal);
+    $("#finalTotal").text(userTotal); 
+          //sets win/lose conditions
+        if (userTotal == Random){
+          yaywin();
         }
-
-        var wSpan = $('<span>').text(wins);
-        var lSpan = $('<span>').text(losses);
-
-        var pWins = $('<p>').text('Wins: ');
-        var pLosses = $('<p>').text('Losses: ');
-
-        pWins.append(wSpan);
-        pLosses.append(lSpan);
-
-        $('#winArea').append(pWins);
-        $('#winArea').append(pLosses);
-    }
-
-    function rendergems() {
-        //render gems
-        for (var key in gems) {
-            var gemDiv = $("<div class='gems-button' data-name='" + key + "'>");
-            var gemImg = $("<img alt='image' class='gem-img'>").attr("src", gems[key].imageUrl);
-            gemDiv.append(gemImg);
-            $("#gem-area").append(gemDiv);
+        else if ( userTotal > Random){
+          loser();
+        }   
+  })  
+  $("#two").on ("click", function(){
+    userTotal = userTotal + num2;
+    console.log('New userTotal= ' + userTotal);
+    $("#finalTotal").text(userTotal); 
+        if (userTotal == Random){
+          yaywin();
         }
-    }
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
+  $("#three").on ("click", function(){
+    userTotal = userTotal + num3;
+    console.log('New userTotal= ' + userTotal);
+    $("#finalTotal").text(userTotal);
 
-    function updateMatchingNumber(th) {
-        var self = th;
-
-        if (self.attr('data-name') == 'red') {
-            yourMatchingNumber = yourMatchingNumber + gems[self.attr('data-name')].points;
-        } else if (self.attr('data-name') == 'blue') {
-            yourMatchingNumber = yourMatchingNumber + gems[self.attr('data-name')].points;
-        } else if (self.attr('data-name') == 'yellow') {
-            yourMatchingNumber = yourMatchingNumber + gems[self.attr('data-name')].points;
-        } else {
-            yourMatchingNumber = yourMatchingNumber + gems[self.attr('data-name')].points;
+//sets win/lose 
+          if (userTotal == Random){
+          yaywin();
         }
-    }
-
-    function renderMatchingNumber() {
-        var scoreNumDiv = $("<div id='score-number'>").text(yourMatchingNumber);
-        $("#score-area").html();
-        $("#score-area").html(scoreNumDiv);
-    }
-
-    setGame();
-    updateDom();
-    rendergems();
-    renderMatchingNumber();
-
-    //on.click event 
-    $(".gems-button").on("click", function(event) {
-        updateMatchingNumber($(this));
-        renderMatchingNumber();
-
-        //Check result
-        if (yourMatchingNumber == randomNum) {
-            wins++;
-            setGame();
-            updateDom(true);
-        } else if (yourMatchingNumber > randomNum) {
-            losses++;
-            setGame();
-            updateDom(false);
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
+  $("#four").on ("click", function(){
+    userTotal = userTotal + num4;
+    console.log('New userTotal= ' + userTotal);
+    $("#finalTotal").text(userTotal); 
+      
+          if (userTotal == Random){
+          yaywin();
         }
-    });
+        else if ( userTotal > Random){
+          loser();
+        }
+  });   
+}); 
